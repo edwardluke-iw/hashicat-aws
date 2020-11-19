eccfa0a88cc5848773873123cf550884d9b05d07
Edward Luke <edward.luke@infinityworks.com>
2020-11-19 11:13:05 +0000

    Adding slogan

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. Hunting that Wile E. Coyote circa 1930
   <!-- END -->
 
   </div>
